bcc64ba6fe0c7415dba27de14fa6d16160bfd62d
Racheal Appiah-kubi <coderacheal@gmail.com>
2024-06-21 18:07:56 +0000

    Finalise authentication for app using `streamlit-authentictor`

---
+++ Utils/info.py
@@ -0,0 +1,27 @@
+column_1 = """
+### Attrition Insight
+Attrition Insight is a Machine Learning application that  predicts the likelihood of an employee to leave the company based on various demographic and job-related factors.
+
+### Key Features
+- **View Data:** Access proprietory data from IBM.
+- **Dashboard:** Explore interactive data visualizations for insights.
+- **Real-time Prediction:** Instantly see predictions for employee attrition.
+- **History:** See past predictions made.
+
+### User Benefits
+- **Data-driven Decisions:** Make informed decisions backed by data analytics.
+- **Easy Machine Learning:** Utilize powerful machine learning algorithms effortlessly.
+- **Live Demo:** Watch a demo video to see the app in action.
+
+[Watch Demo Video](link)
+"""
+
+column_2 = """
+### Machine Learning Integration
+- **Model Selection:** Choose between two advanced models for accurate predictions.
+- **Seamless Integration:** Integrate predictions into your workflow with a user-friendly interface.
+- **Probability Estimates:** Gain insights into the likelihood of predicted outcomes.
+
+### Need Help?
+For collaborations contact me at [hello@rachealappiahkubi.com](mailto:hello@rachealappiahkubi.com).
+"""
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from Utils.info import column_1, column_2
 
 
 st.set_page_config(
@@ -7,3 +11,37 @@ st.set_page_config(
     page_icon='🏠'
 )
 
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+name, authentication_status, username = authenticator.login(location='sidebar')
+
+
+if st.session_state['authentication_status']:
+    authenticator.logout(location='sidebar')
+    st.title('Attrition Calculator')
+
+    col1, col2 = st.columns(2)
+    with col1:
+        column_1
+    with col2:
+        column_2
+
+elif st.session_state['authentication_status'] is False:
+    st.error('Wrong username/password')
+elif st.session_state['authentication_status'] is None:
+    st.info('Login to get access to the app')
+    st.code("""
+    Test Account
+    Username: coderacheal
+    Password: 123456
+    """)
